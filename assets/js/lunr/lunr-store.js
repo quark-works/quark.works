1a574922410bcc86499bc075e685dedcf6062d59
Brandon <brandon.abr@hotmail.com>
2025-08-09 20:28:36 -0600

    Added delaevery v4n

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,21 +1,27 @@
 var store = [{
         "title": "BaulT",
-        "excerpt":"Wireless drop-in replacement PCB for Vault35 cases      Features:     Powered by a nrf52840 in a E73-2G4M08S1C module.   LED charge indicator and power switch.   Boost pad on the back for faster charging.   Runs ZMK firmware.   Firmware:     ZMK Config   User config   Availability:     For sale in CBKBD.  ","categories": [],
+        "excerpt":"Wireless drop-in replacement PCB for Vault35 cases      Features:     Powered by a nrf52840 in a E73-2G4M08S1C module.   LED charge indicator and power switch.   Boost pad on the back for faster charging.   Runs ZMK firmware.   Firmware:     ZMK Config   User config   Availability:     For sale in CBKBD.   Pictures        ","categories": [],
         "tags": [],
         "url": "/portfolio/2024-11-28-bault/",
         "teaser": "/assets/pcbs/baultbottom.png"
       },{
         "title": "Acai PCB",
-        "excerpt":"10u ortho PCB with RGB per key      Features:     Powered by a rp2040 IC.   RGB per key with RGB matrix implementation.   Runs QMK firmware, VIAL compatible.   Firmware:     QMK Config   Availability:     Acai was available in CBKBD.  ","categories": [],
+        "excerpt":"10u ortho PCB with RGB per key      Features:     Powered by a rp2040 IC.   RGB per key with RGB matrix implementation.   Runs QMK firmware, VIAL compatible.   Firmware:     QMK Config   Availability:     Acai PCBs are available in CBKBD.   Pictures        ","categories": [],
         "tags": [],
         "url": "/portfolio/2025-02-12-acai/",
-        "teaser": "/assets/pcbs/acaiback.png"
+        "teaser": "/assets/pcbs/acaitop.png"
       },{
         "title": "QAZ RP2040",
         "excerpt":"QAZ PCB with an integrated rp2040      Features:     Powered by a rp2040 IC.   Available in MX hotswap and soldered, as well as a ks-33 variant.   Runs QMK firmware, VIAL compatible.   Firmware:     QMK Config   Availability:     Available in CBKBD.  ","categories": [],
         "tags": [],
         "url": "/portfolio/2025-02-12-qazrp2040/",
         "teaser": "/assets/pcbs/qazlayers.png"
+      },{
+        "title": "DeLAEvery-Van",
+        "excerpt":"LAE Enter Minivan + V4N      Features:     Powered by a rp2040 IC.   3 LED layer indicators.   Snappable top row.   Hotswap.   Runs QMK firmware, VIAL compatible.   Availability:     For sale in Dash Keebs.   Pictures        ","categories": [],
+        "tags": [],
+        "url": "/portfolio/2025-08-09-delaeveryvan/",
+        "teaser": "/assets/pcbs/delaeverybottom.png"
       },{
         "title": "Hello, world!",
         "excerpt":"This is a sample post to have something in my main page.  ","categories": [],
